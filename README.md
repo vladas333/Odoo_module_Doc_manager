084be940d4926abe99f135a73c65824175bd8638
unknown <vladas.budvytis@gmail.com>
2024-02-04 22:41:15 +0200

    First push

---
+++ README.md
@@ -0,0 +1,1 @@
+# Odoo_module_Doc_mananger
---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Document managment tool",
+    "version": "13.0.1.0.0",
+    "sequence": 30,
+    "author": "V. Budvytis",
+    "category": "Extra Tools",
+    'summary': 'Store information about various documents or books',
+    'description': 'A module to manage documents or books with: name, description and company information.',
+    'depends': ['base'],
+    'data': [
+        'views/document_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
